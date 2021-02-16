2cd5b6d3d400d067c32fe87d0557c6ab58502014
pramudyapolinema <1941720054@student.polinema.ac.id>
2021-02-16 09:42:15 +0700

    02_praktikum_web_lanjut_satu

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Nama : Pramudya Wibowo <br> NIM : 1941720054";
+});
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan id $id";
 });
